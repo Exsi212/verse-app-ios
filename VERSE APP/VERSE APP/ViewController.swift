024342c914d2798412f7eb5ca8895e7dc1e8528f
Olya Aleshina <39920514+Exsi212@users.noreply.github.com>
2023-10-31 11:57:40 +0300

    Add files via upload

---
+++ VERSE APP/VERSE APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VERSE APP
+//
+//  Created by Ольга Алешина on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+
+
+}
+
